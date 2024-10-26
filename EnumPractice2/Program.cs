cfc710a4998d83c18a7c58b1d6ad90974c7c3a2c
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-10-26 20:41:35 -0300

    4 commit

---
+++ EnumPractice2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    enum Meses
+    {
+        Janeiro = 1,
+        Fevereiro = 2,
+        Marco = 3,
+        Abril = 4,
+        Maio = 5,
+        Junho = 6,
+        Julho = 7,
+        Agosto = 8,
+        Setembro = 9,
+        Outubro = 10,
+        Novembro = 11,
+        Dezembro = 12
+    }
+
+    static void Main(string[] args)
+    {
+        // Utilizando o enum
+        Meses mesAtual = Meses.Outubro;
+
+        // Exibe o nome e o valor do mês
+        Console.WriteLine($"Estamos no mês de: {mesAtual}");  // Saída: Estamos no mês de: Outubro
+        Console.WriteLine($"Número do mês: {(int)mesAtual}"); // Saída: Número do mês: 10
+    }
+}
---
+++ ExemploCondicional/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ExemploCondicional
+{
+    static void Main(string[] args)
+    {
+        int idade = 20;
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Voce e maior de idade.");
+        }
+        else if (idade >= 16 && idade < 18)
+        {
+            Console.WriteLine("Voce pode votar, mas ainda nao e maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Voce e menor de idade.");
+        }
+    }
+}

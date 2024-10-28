f3799c4f6fd2bd4e4fc5f2a3a99eeff3cba8fc4e
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-10-27 21:56:21 -0300

    extra commit

---
+++ EstruturaRepeticao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ExemploRepeticao
+{
+    static void Main(string[] args)
+    {
+        // Usado 'for' para contar ate um numero informado pelo usuario
+        Console.WriteLine("Digite um numero para contar de 1 ate esse numero:");
+        if(int.TryParse(Console.ReadLine(), out int limitefor) && limitefor > 0)
+        {
+            
+        }
+    }
+}

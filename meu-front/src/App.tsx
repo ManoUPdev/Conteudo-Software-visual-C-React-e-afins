333914dc694c597d7a4b029af5d7c20b743ea384
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-10-31 10:20:54 -0300

    13 commit includes front

---
+++ meu-front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponenteExemplo from './ComponenteExemplo'
+import ConsultarCEP from './ConsultarCEP';
+function App() {
+  return (
+    <div> 
+        <ConsultarCEP></ConsultarCEP>
+    </div>
+  );
+}
+
+export default App;
---
+++ meu-front/src/ConsultarCEP.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+
+function ConsultarCEP(){
+
+
+
+    useEffect(() => {
+        //Metodo utilizado para executar qualquer codigo enquanto
+        //o componente está sendo aberto ou renderizado
+        //Biblioteca de requisitos - AXIOS
+        fetch("https://viacep.com.br/ws/01001000/json/")
+        .then(resposta => {
+            return resposta.json();
+        })
+        .then(cep => {
+        console.log(cep);
+        });
+
+        
+    });
+        
+    
+
+
+return(
+    <div>
+        <h1>Consultar CEP </h1>
+
+    </div>
+);
+}
+export default ConsultarCEP;
+
+//EXERCÍCIOS
+//1 - Exibir os dados do CEP no HTML
+//2 - Realizar a requisição para a sua API
+//3 - Resolver o problema de CORS na API
+//4 - Exibir a lista de produtos no HTML
